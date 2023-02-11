b9dfa4afca91688918db587efee87e89867fb725
SandunGit <125024410+SandunGit@users.noreply.github.com>
2023-02-11 20:15:35 +0530

    Update delete-lead.php

--- tritech-crm-main/tritech-crm/manage-lead/delete-lead.php
+++ tritech-crm-main/tritech-crm/manage-lead/delete-lead.php
@@ -2,11 +2,13 @@
     include('../config/constants.php');
 
     $id = $_GET['id'];
-    $sql = "DELETE FROM tbl_lead WHERE id=$id";
+    $sql = "DELETE FROM tbl_lead WHERE id=?";
+    
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
 
-    $res = mysqli_query($conn, $sql);
-
-    if ($res==TRUE)
+    if (mysqli_stmt_affected_rows($stmt) > 0)
     {
         $_SESSION['delete'] = "<div class='alert-success'> Lead Removed Successfully </div>";
         header('location: http://localhost/tritech-crm/manage-lead/manage-lead.php');
@@ -16,4 +18,7 @@
         $_SESSION['delete']= "<div class='alert-failed'> Failed To Remove Lead </div>";
         header('location: http://localhost/tritech-crm/manage-lead/manage-lead.php');
     }
+    
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
 ?>
