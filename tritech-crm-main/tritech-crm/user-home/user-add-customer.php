c21e8809550a948c1e85c6686e88b1c20e531795
SandunGit <125024410+SandunGit@users.noreply.github.com>
2023-02-11 20:38:43 +0530

    Update user-add-customer.php

--- tritech-crm-main/tritech-crm/user-home/user-add-customer.php
+++ tritech-crm-main/tritech-crm/user-home/user-add-customer.php
@@ -56,38 +56,27 @@
     <!-- Main Content End -->
 
 <?php include('../common/footer.php'); ?> 
+<?php
+if(isset($_POST['submit'])) {
+    $full_name = $_POST['full_name']; 
+    $reg_date = date('y-m-d', strtotime($_POST['reg_date']));
+    $contact = $_POST['contact'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
 
-<?php 
+    $sql = "INSERT INTO tbl_customer (full_name, reg_date, contact, address, email) VALUES (?,?,?,?,?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, 'sssss', $full_name, $reg_date, $contact, $address, $email);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_affected_rows($stmt);
+    mysqli_stmt_close($stmt);
 
-    if(isset($_POST['submit'])) //whether the submit value is passed
-    {
-        $full_name = $_POST['full_name']; //Get Values from form
-        $reg_date = date('y-m-d',strtotime($_POST['reg_date']));
-        $contact = $_POST['contact'];
-        $email = $_POST['email'];
-        $address = $_POST['address'];          
-
-        $sql = "INSERT INTO tbl_customer SET
-            full_name = '$full_name',
-            reg_date = '$reg_date',
-            contact= '$contact',
-            address='$address',
-            email='$email'
-        ";
-        
-        $res = mysqli_query($conn, $sql) or die(mysqli_error($conn)); //execute query
-
-        if($res==TRUE)
-        {
-            $_SESSION['add'] = "<div class='alert-success'> Customer Successfully Added </div>";
-            header('location: http://localhost/tritech-crm/user-home/user-customer.php'); 
-        }
-        else
-        {
-            $_SESSION['add'] = "<div class='alert-failed'>Failed to Add Customer </div> ";
-            header('location: http://localhost/tritech-crm/user-home/user-customer.php'); 
-        }  
+    if ($result) {
+        $_SESSION['add'] = "<div class='alert-success'> Customer Successfully Added </div>";
+        header('location: http://localhost/tritech-crm/user-home/user-customer.php');
+    } else {
+        $_SESSION['add'] = "<div class='alert-failed'>Failed to Add Customer </div> ";
+        header('location: http://localhost/tritech-crm/user-home/user-customer.php');
     }
-
-
+}
 ?>
