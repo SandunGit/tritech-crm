7d3bb524892257c7425e11f55ea19591ce313459
SandunGit <125024410+SandunGit@users.noreply.github.com>
2023-02-11 19:54:10 +0530

    Update delete-campaign.php

--- tritech-crm-main/tritech-crm/manage-campaign/delete-campaign.php
+++ tritech-crm-main/tritech-crm/manage-campaign/delete-campaign.php
@@ -1,12 +1,14 @@
 <?php
     include('../config/constants.php');
 
-    $id = $_GET['id'];
-    $sql = "DELETE FROM tbl_campaign WHERE id=$id";
+    $id = intval($_GET['id']);
+    $sql = "DELETE FROM tbl_campaign WHERE id=?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $id);
 
-    $res = mysqli_query($conn, $sql);
+    $res = mysqli_stmt_execute($stmt);
 
-    if ($res==TRUE)
+    if ($res)
     {
         $_SESSION['delete'] = "<div class='alert-success'> Campaign Deleted Successfully </div>";
         header('location: http://localhost/tritech-crm/manage-campaign/manage-campaign.php');
