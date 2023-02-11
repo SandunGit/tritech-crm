ff5811657893ba1f667a08bf2bf6ac6fdd85808d
SandunGit <125024410+SandunGit@users.noreply.github.com>
2023-02-11 19:24:56 +0530

    Update convert-lead.php

--- tritech-crm-main/tritech-crm/user-home/convert-lead.php
+++ tritech-crm-main/tritech-crm/user-home/convert-lead.php
@@ -1,34 +1,36 @@
 <?php
      include('../config/constants.php');
      include('../config/login-check.php');
-        $id=$_GET['id'];
 
+     $id = (int)$_GET['id']; //casting the variable to int type to prevent SQL injection
 
-        $sql2 = "INSERT INTO tbl_customer (full_name,email,contact,reg_date,type)  
-                 SELECT full_name,contact,email,date,'converted'
-                 FROM tbl_lead   
-                 WHERE id=$id
-                "; //copying data from table lead to customer table
+     $sql2 = "INSERT INTO tbl_customer (full_name,email,contact,reg_date,type)  
+              SELECT full_name,contact,email,date,'converted'
+              FROM tbl_lead   
+              WHERE id=?";  //using placeholder instead of directly inserting the $id variable
 
-            $res2 = mysqli_query($conn,$sql2);
+     // preparing and executing the query
+     $stmt = mysqli_prepare($conn, $sql2);
+     mysqli_stmt_bind_param($stmt, "i", $id);
+     $res2 = mysqli_stmt_execute($stmt);
 
-            if($res2 == TRUE)
-            {
-                $_SESSION ['update'] = "<div class='alert-success'>Lead Added to Customers</div>";
-                header('location: http://localhost/tritech-crm/user-home/manage-lead.php');
+     if($res2)
+     {
+        $_SESSION['update'] = "<div class='alert-success'>Lead Added to Customers</div>";
+        header('location: http://localhost/tritech-crm/user-home/manage-lead.php');
+     }
+     else
+     {
+        $_SESSION['update'] = "<div class='alert-failed'>Failed to add Lead to Customers </div> ";
+        header('location: http://localhost/tritech-crm/user-home/manage-lead.php');
+     }
 
-            }
-            else
-            {
-                $_SESSION['update'] = "<div class='alert-failed'>Failed to add Lead to Customers </div> ";
-                header('location: http://localhost/tritech-crm/user-home/manage-lead.php');
+     $sql = "DELETE FROM tbl_lead WHERE id=?";  //using placeholder instead of directly inserting the $id variable
 
-            }
-
-            $sql = "DELETE FROM tbl_lead WHERE id=$id";  //Delete the lead after converting to customer
-
-            $res = mysqli_query($conn, $sql);
-
+     // preparing and executing the query
+     $stmt = mysqli_prepare($conn, $sql);
+     mysqli_stmt_bind_param($stmt, "i", $id);
+     $res = mysqli_stmt_execute($stmt);
 ?>
 
 
